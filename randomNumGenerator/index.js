38584d45b1972d17097be5789f0316ebec7844a1
Rodolfo Marques <rodolfo@splyt.com>
2019-08-11 15:55:32 +0100

    configurable port

--- randomNumGenerator/index.js
+++ randomNumGenerator/index.js
@@ -15,7 +15,7 @@ server.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await server.listen(3000);
+    await server.listen(process.env.SERVER_PORT);
     server.log.info(`server listening on ${ server.server.address().port }`);
   } catch (err) {
     server.log.error(err);
