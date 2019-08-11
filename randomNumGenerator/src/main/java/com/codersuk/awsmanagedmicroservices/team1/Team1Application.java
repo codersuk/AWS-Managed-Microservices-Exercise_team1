178b0bcf3ec8bcbbf9919209a238322b2ebb0a51
Rodolfo Marques <rodolfo@splyt.com>
2019-08-11 14:22:18 +0100

    replaced old folder

---
+++ randomNumGenerator/src/main/java/com/codersuk/awsmanagedmicroservices/team1/Team1Application.java
@@ -0,0 +1,13 @@
+package com.codersuk.awsmanagedmicroservices.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team1Application.class, args);
+	}
+
+}
